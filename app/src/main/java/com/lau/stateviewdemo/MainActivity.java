cb0f54597f17942b4654aad85d71bcdcd5e578d1
crayonxiaoxin <lau@efortunetech.com>
2019-08-07 12:09:29 +0800

    commit

--- app/src/main/java/com/lau/stateviewdemo/MainActivity.java
+++ app/src/main/java/com/lau/stateviewdemo/MainActivity.java
@@ -1,14 +1,48 @@
 package com.lau.stateviewdemo;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.lau.StateView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private StateView stateView;
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        stateView = findViewById(R.id.stateView);
+        stateView.setRetryListener(new StateView.setOnRetry() {
+            @Override
+            public void onRetry(View retryView) {
+                retryView.findViewById(R.id.base_retry).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // 模拟重试后加载成功
+                        stateView.showView(StateView.State.LOADING);
+                        mHandler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                stateView.showView(StateView.State.CONTENT);
+                            }
+                        }, 2000);
+                    }
+                });
+            }
+        });
+        // 模拟加载失败
+        stateView.showView(StateView.State.LOADING);
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                stateView.showView(StateView.State.RETRY);
+            }
+        }, 2000);
     }
 }
